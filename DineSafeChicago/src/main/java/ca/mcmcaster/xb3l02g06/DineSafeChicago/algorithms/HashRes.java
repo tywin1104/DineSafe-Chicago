48e7fbe641256d183d6e6f709081a851d5c4652a
Naohiro <shens12@mcmaster.ca>
2019-03-27 23:42:26 -0400

    Add HashRes and complementary linkedlist

---
+++ DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/algorithms/HashRes.java
@@ -0,0 +1,125 @@
+package main;
+
+import java.util.*;
+
+public class LinkedList<AnyType> implements Iterable<AnyType>
+{
+	/**
+	 * State variables
+	 */
+   private Node<AnyType> head;
+   private int size;
+
+ /**
+   *  Constructs an empty list
+   */
+   public LinkedList()
+   {
+      head = null;
+      size = 0;
+   }
+
+   public int getSize() {
+	   return size;
+   }
+   
+   public void addFirst(AnyType item)
+   {
+      head = new Node<AnyType>(item, head);
+      size ++;
+   }
+
+   
+   public void addLast(AnyType item)
+   {
+      if( head == null)
+         addFirst(item);
+      else
+      {
+         Node<AnyType> tmp = head;
+         while(tmp.next != null) tmp = tmp.next;
+
+         tmp.next = new Node<AnyType>(item, null);
+         size ++;
+      }
+
+   }
+ 
+   public AnyType get(int pos)
+   {
+      if (head == null) throw new IndexOutOfBoundsException();
+
+      Node<AnyType> tmp = head;
+      for (int k = 0; k < pos; k++) tmp = tmp.next;
+
+      if( tmp == null) throw new IndexOutOfBoundsException();
+
+      return tmp.data;
+   }
+ /**
+   *  Returns a string representation
+   *
+   */
+   public String toString()
+   {
+      StringBuffer result = new StringBuffer();
+      for(Object x : this)
+      	result.append(x + " ");
+
+      return result.toString();
+   }
+ 
+ /*******************************************************
+ *
+ *  The Node class
+ *
+ ********************************************************/
+   private static class Node<AnyType>
+   {
+      private AnyType data;
+      private Node<AnyType> next;
+
+      public Node(AnyType data, Node<AnyType> next)
+      {
+         this.data = data;
+         this.next = next;
+      }
+   }
+
+ /*******************************************************
+ *
+ *  The Iterator class
+ *
+ ********************************************************/
+
+   public Iterator<AnyType> iterator()
+   {
+      return new LinkedListIterator();
+   }
+
+   private class LinkedListIterator  implements Iterator<AnyType>
+   {
+      private Node<AnyType> nextNode;
+
+      public LinkedListIterator()
+      {
+         nextNode = head;
+      }
+
+      public boolean hasNext()
+      {
+         return nextNode != null;
+      }
+
+      public AnyType next()
+      {
+         if (!hasNext()) throw new NoSuchElementException();
+         AnyType res = nextNode.data;
+         nextNode = nextNode.next;
+         return res;
+      }
+
+      public void remove() { throw new UnsupportedOperationException(); }
+   }
+
+}
---
+++ DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/algorithms/LinkedList.java
@@ -0,0 +1,92 @@
+package main;
+
+import main.Restaurant;
+import main.Inspection.ResultT;
+import main.LinkedList;
+
+import java.util.ArrayList;
+
+public class HashRes {
+	
+	private ArrayList<LinkedList<Restaurant>> hashTable;
+	
+	// Constructor
+	public HashRes() {
+		this.hashTable = new ArrayList<LinkedList<Restaurant>>();
+		for(int i=0; i<100; i++) {
+			this.hashTable.add(new LinkedList<Restaurant>());
+		}
+	}
+	
+	public void convertToHash(ArrayList<Restaurant> resList) {
+		for (int i=0; i<resList.size(); i++) {
+			insertRes(resList.get(i));
+		}
+	}
+	
+	public void insertRes(Restaurant res) {
+		// Compute the index using Hash function
+		int index = hashFunc(res.getZip());
+		// Insert the element in the linked list at the particular index
+		this.hashTable.get(index).addLast(res);
+	}
+	
+	public int hashFunc(int zip) {
+		return zip%100;
+	}
+	
+	public ArrayList<Restaurant> searchZip (int zip){
+		// Create an ArrayList for restaurants having the same zipcode
+		ArrayList<Restaurant> resSameZip = new ArrayList<Restaurant>();
+		// Compute the index by using the hash function
+		int index = hashFunc(zip);
+		// Search the linked list at the specific index
+		for(int i=0; i<hashTable.get(index).getSize(); i++) {
+			if(hashTable.get(index).get(i).getZip() == zip) {
+				resSameZip.add(hashTable.get(index).get(i));
+			}
+		}
+		return resSameZip;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		/*
+		Inspection inspection1 = new Inspection(ResultT.CONDITIONALPASS,"1 PERSON MAINTAIN","01/02/2018");
+		Inspection inspection2 = new Inspection(ResultT.INVALID,"41 WIPING CLOTAIN","03/05/2019");
+		Inspection inspection3 = new Inspection(ResultT.PASS,"","12/05/2018");
+		Inspection inspection4 = new Inspection(ResultT.FAIL,"23 aaaaaa","03/05/2017");
+		
+		Restaurant res1 = new Restaurant ("aaa", 1114, 1141, 114.1, 411.1);
+		Restaurant res2 = new Restaurant("aab", 1121, 1131, 121.1, 121.1);
+		Restaurant res3 = new Restaurant("aas", 1131, 1141, 131.1, 131.1);
+		Restaurant res4 = new Restaurant("aaf", 1111, 1121, 111.1, 121.1);
+		
+		res1.addInspection(inspection1);
+		res1.addInspection(inspection2);
+		res1.addInspection(inspection3);
+		res1.addInspection(inspection4);
+		
+		res2.addInspection(inspection2);
+		res2.addInspection(inspection1);
+		
+		res3.addInspection(inspection4);
+		
+		res4.addInspection(inspection3);
+		res4.addInspection(inspection4);
+		res4.addInspection(inspection1);
+		
+		ArrayList<Restaurant> resList = new ArrayList<Restaurant>();
+		resList.add(res1);
+		resList.add(res2);
+		resList.add(res3);
+		resList.add(res4);
+		HashRes hashTable = new HashRes();
+		hashTable.convertToHash(resList);
+		System.out.println(hashTable.searchZip(1141));
+		*/
+	}
+
+}
