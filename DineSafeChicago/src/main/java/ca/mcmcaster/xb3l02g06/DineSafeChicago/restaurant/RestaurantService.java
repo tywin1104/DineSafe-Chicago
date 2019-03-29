c8b9cc3c92aee6a95608409a91e0bc29694e441d
Tywin <tiaven1104@gmail.com>
2019-03-29 12:09:09 -0400

    test sort, modify compareTo

--- DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/restaurant/RestaurantService.java
+++ DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/restaurant/RestaurantService.java
@@ -1,11 +1,13 @@
 package ca.mcmcaster.xb3l02g06.DineSafeChicago.restaurant;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ca.mcmcaster.xb3l02g06.DineSafeChicago.algorithms.HashTable;
+import ca.mcmcaster.xb3l02g06.DineSafeChicago.algorithms.HeapSort;
 import ca.mcmcaster.xb3l02g06.DineSafeChicago.inspection.InspectionRepository;
 
 @Service
@@ -23,6 +25,9 @@ public class RestaurantService {
 		HashTable hashTable = new HashTable(59);  
 		hashTable.loadRestaurants(restaurants);
 		ArrayList<Restaurant> filtered = hashTable.getRestaurants(zip);
+		Collections.sort(filtered);
+		 
+//		HeapSort.sortHeap(filtered, filtered.size()); 
 		return filtered; 
 	}
 	
