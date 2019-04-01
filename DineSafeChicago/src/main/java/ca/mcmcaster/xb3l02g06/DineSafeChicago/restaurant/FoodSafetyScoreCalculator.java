a3a9453c31a461b06151548bf168992445376faa
Naohiro <shens12@mcmaster.ca>
2019-04-01 15:27:52 -0400

    Rearrange for new interface

--- DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/restaurant/FoodSafetyScoreCalculator.java
+++ DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/restaurant/FoodSafetyScoreCalculator.java
@@ -2,11 +2,24 @@ package ca.mcmcaster.xb3l02g06.DineSafeChicago.restaurant;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Calendar;
 
 import ca.mcmcaster.xb3l02g06.DineSafeChicago.inspection.Inspection;
 
+/**
+ * This class serves as a calculator for foodSafetyScore of the input restaurant
+ * @author shens12
+ * @version April 1, 2019
+ *
+ */
 public class FoodSafetyScoreCalculator {
 
+	/**
+	 * Calculate the foodSafetyScore for the restaurant
+	 * @param restaurant - the input restaurant whose foodSafetyScore will be calculated
+	 * @return foodSafetyScore - the score of the input restaurant
+	 * 
+	 */
 	public static double calculate(Restaurant restaurant) {
 		double foodSafetyScore = 0;
 		double RECENT_INSPECTION = 20;
@@ -45,6 +58,11 @@ public class FoodSafetyScoreCalculator {
 		return foodSafetyScore;
 	}
 
+	/**
+	 * Sort all inpections of the imput restaurant by inspection time from newest to oldest
+	 * Selection sort is adopted because of the limited number of inspections per restaurant
+	 * @param restaurant - input restaurant
+	 */
 	private static void sortInspections(Restaurant restaurant) {
 		int n = restaurant.getInspections().size();
 		for (int i = 0; i < n - 1; i++) {
@@ -62,12 +80,24 @@ public class FoodSafetyScoreCalculator {
 
 	}
 
+	/**
+	 * Check if the input inspection is performed within one year from today
+	 * @param latestInspection - newest inspection record performed on the specific restaruant
+	 * @return boolean value - If the input inspection is performed within one year from today, return true., otherwise return false
+	 */
 	private static boolean recentInspection(Inspection latestInspection) {
-		@SuppressWarnings("deprecation")
-		Date recentDate = new Date(2018, 4, 1);
-		return latestInspection.getTime().after(recentDate);
+		Calendar cal = Calendar.getInstance();
+		Date today = cal.getTime();
+		cal.add(Calendar.YEAR,-1);
+		Date lastYear = cal.getTime();
+		return latestInspection.getTime().after(lastYear);
 	}
 
+	/**
+	 * calculate the pass rate (including pass and conditional pass) of the restaurant calculated by numbers of passes divided by total number of inspections
+	 * @param array - an array of inspections
+	 * @return double value - pass rate
+	 */
 	private static double passRate(List<Inspection> array) {
 		int pass = 0;
 		for (int i = 0; i < array.size(); i++) {
@@ -75,9 +105,16 @@ public class FoodSafetyScoreCalculator {
 				pass++;
 			}
 		}
-		return (double) (pass / array.size());
+		return ((double)pass / array.size());
 	}
 
+	/**
+	 * calculate the weighted grade based on at most five latest inspections performed on the specific restaurant
+	 * Newest inspections will be assigned more weight proportion
+	 * @param numberOfNewInspections - an integer representing the number of inspections to be considered
+	 * @param restaurant - input restaurant whose weighted grade will be calculated
+	 * @return weightedGrade - overall grades based on results of latest inspections
+	 */
 	private static double weightedGrade(int numberOfNewInspections, Restaurant restaurant) {
 		double weightedGrade = 0;
 		double denominator = (1 + numberOfNewInspections) * numberOfNewInspections / 2.0;
