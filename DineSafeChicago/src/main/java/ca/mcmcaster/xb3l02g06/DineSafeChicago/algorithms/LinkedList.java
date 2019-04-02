adb418bbc4f6ff44932713c5650f902239e23a5e
Naohiro <shens12@mcmaster.ca>
2019-04-01 22:14:45 -0400

    add doc

--- DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/algorithms/LinkedList.java
+++ DineSafeChicago/src/main/java/ca/mcmcaster/xb3l02g06/DineSafeChicago/algorithms/LinkedList.java
@@ -2,6 +2,13 @@ package ca.mcmcaster.xb3l02g06.DineSafeChicago.algorithms;
 
 import java.util.*;
 
+/**
+ * a linked list data structure for hash table
+ * @author Shanghong Shen
+ * @version April 1, 2019
+ * @param <AnyType> - parameterized type to be contained in the linked list
+ */
+
 public class LinkedList<AnyType> implements Iterable<AnyType> {
 	/**
 	 * State variables
@@ -17,15 +24,27 @@ public class LinkedList<AnyType> implements Iterable<AnyType> {
 		size = 0;
 	}
 
+	/**
+	 * Get the size of the linked list
+	 * @return size - an integer indicating the number of elements contained in the linked list
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * add the input item to the beginning of the linked list
+	 * @param item
+	 */
 	public void addFirst(AnyType item) {
 		head = new Node<AnyType>(item, head);
 		size++;
 	}
 
+	/**
+	 * add the input item to the end of the linked list
+	 * @param item
+	 */
 	public void addLast(AnyType item) {
 		if (head == null)
 			addFirst(item);
@@ -40,6 +59,11 @@ public class LinkedList<AnyType> implements Iterable<AnyType> {
 
 	}
 
+	/**
+	 * get the item at the input index 
+	 * @param pos - the index indicating the position of the desired item in the linked list
+	 * @return data - the data containing at the input position in the linked list
+	 */
 	public AnyType get(int pos) {
 		if (head == null)
 			throw new IndexOutOfBoundsException();
@@ -55,7 +79,7 @@ public class LinkedList<AnyType> implements Iterable<AnyType> {
 	}
 
 	/**
-	 * Returns a string representation
+	 * Returns a string representation of the linked list
 	 *
 	 */
 	public String toString() {
@@ -66,42 +90,59 @@ public class LinkedList<AnyType> implements Iterable<AnyType> {
 		return result.toString();
 	}
 
-	/*******************************************************
-	 *
-	 * The Node class
-	 *
-	 ********************************************************/
+/**
+ * 
+ * @author Shanghong Shen
+ * @version April 1, 2019
+ * @param <AnyType> - A parameterized type to be contained in the data part of the node
+ * 
+ */
 	private static class Node<AnyType> {
 		private AnyType data;
 		private Node<AnyType> next;
 
+		/**
+		 * Constructor of the Node class
+		 * @param data - data to be contained in the node
+		 * @param next - a reference to the next node in the linked list
+		 */
 		public Node(AnyType data, Node<AnyType> next) {
 			this.data = data;
 			this.next = next;
 		}
 	}
 
-	/*******************************************************
-	 *
-	 * The Iterator class
-	 *
-	 ********************************************************/
 
+	/**
+	 * The iterator class
+	 */
 	public Iterator<AnyType> iterator() {
 		return new LinkedListIterator();
 	}
 
 	private class LinkedListIterator implements Iterator<AnyType> {
 		private Node<AnyType> nextNode;
-
+		
+		/**
+		 * Constructor fot the iterator of the linked list
+		 */
 		public LinkedListIterator() {
 			nextNode = head;
 		}
 
+		/**
+		 * Check if the current node is at the end of the linked list
+		 * @return boolean -  true if current node is not the end of the linked list, otherwise false
+		 */
 		public boolean hasNext() {
 			return nextNode != null;
 		}
 
+		/**
+		 * return the next element
+		 * @return res - next node
+		 * @throws java.lang.Exception
+		 */
 		public AnyType next() {
 			if (!hasNext())
 				throw new NoSuchElementException();
@@ -110,6 +151,9 @@ public class LinkedList<AnyType> implements Iterable<AnyType> {
 			return res;
 		}
 
+		/**
+		 * clean the current linked list
+		 */
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
